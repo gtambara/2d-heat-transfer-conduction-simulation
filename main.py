7188b46632e2dea5bd12b35a407e6ff410dd393b
Gabriel Tambara <34486353+gtambara@users.noreply.github.com>
2021-08-18 11:21:10 -0300

    Comentário de auxilio ao usuário

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ Temp = np.zeros((intervalo+1,intervalo+1), dtype = float)
 # Criando matriz T para um instante arbitrário seguinte ao da matriz Temp
 Temp_new = np.copy(Temp)
 
+# Descomentar as linhas abaixo conforme desejar usar as funções. Por padrão, a maioria está comentada para reduzir uso computacional.
+
 #modulo.simulacao(intervalo,comprimento,alpha,Temp,Temp_new,delta_t)
 
 #modulo.mostrar(intervalo,comprimento,Temp_new)
